dca0ce743f2aa5e87336f7091da27450e2d88a05
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2024-09-02 08:13:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ def main():
     print("Hello, World!")
     random_number = random.randint(0, 8)
     print(f"this is my random number= {random_number}")
+    
+    test_min_jumps()
 
 if __name__ == "__main__":
     main()
